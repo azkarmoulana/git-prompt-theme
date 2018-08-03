5d3b99f45a0de1bc971a01b7472d809e37841b46
Azkar Moulana <azkar.moulana@gmail.com>
2018-08-03 10:20:40 +0530

    customized

--- git-prompt.sh
+++ git-prompt.sh
@@ -9,14 +9,14 @@ if test -f ~/.config/git/git-prompt.sh
 then
 	. ~/.config/git/git-prompt.sh
 else
-	PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
+	PS1='\[\033]0;Git-bash: $PWD\007\]' # set window title
 	PS1="$PS1"'\n'                 # new line
-	PS1="$PS1"'\[\033[32m\]'       # change to green
-	PS1="$PS1"'\u@\h '             # user@host<space>
-	PS1="$PS1"'\[\033[35m\]'       # change to purple
-	PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
+	# PS1="$PS1"'\[\033[32m\]'       # change to green
+	# PS1="$PS1"'\u@\h '             # user@host<space>
+	# PS1="$PS1"'\[\033[35m\]'       # change to purple
+	# PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
 	PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
-	PS1="$PS1"'\w'                 # current working directory
+	PS1="$PS1"'\W'                 # current working directory
 	if test -z "$WINELOADERNOEXEC"
 	then
 		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
@@ -33,7 +33,9 @@ else
 	fi
 	PS1="$PS1"'\[\033[0m\]'        # change color
 	PS1="$PS1"'\n'                 # new line
-	PS1="$PS1"'$ '                 # prompt: always $
+	PS1="$PS1"'\[\033[35m\]'       # change to purple
+	PS1="$PS1"'➜ '                 # prompt: always $
+	PS1="$PS1"'\[\033[0m\]'
 fi
 
 MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
